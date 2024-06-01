a6d58e2bfcbf1fa55a194fa5d83535dba53c38c4
simon <simon.schober@outlook.de>
2024-06-01 11:43:05 +0200

    Final version before windows install

--- flake.nix
+++ flake.nix
@@ -23,11 +23,11 @@
         name = "simon"; # name/identifier
         email = ""; # email (used for certain configurations)
         dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
-        theme = "ashes"; # selcted theme from my themes directory (./themes/)
+        theme = "dracula"; # selcted theme from my themes directory (./themes/)
         wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
         # window manager type (hyprland or x11) translator
         wmType = if (wm == "hyprland") then "wayland" else "x11";
-        browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
+        browser = "firefox"; # Default browser; must select one from ./user/app/browser/
         defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
         term = "alacritty"; # Default terminal command;
         font = "Intel One Mono"; # Selected font
